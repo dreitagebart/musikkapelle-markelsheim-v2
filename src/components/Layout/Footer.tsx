59f2117a6883ddd6a9a1035c07d5f8893d6543d2
dreitagebart <stefan.buechold@gmail.com>
2022-06-07 12:58:48 +0200

    update

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Box, Container, Divider, HStack, Icon, Text } from '@chakra-ui/react'
-import { FaCode, FaHeart } from 'react-icons/fa'
+import { FaCode, FaFacebookSquare, FaHeart, FaInstagram } from 'react-icons/fa'
 
 import { FooterMenu } from '..'
 
@@ -18,6 +18,31 @@ export const Footer: React.FC<Props> = () => {
         >
           <Divider></Divider>
           <FooterMenu></FooterMenu>
+          <HStack marginY='.8rem' verticalAlign='center'>
+            <Text>Du findest uns auch auf</Text>
+            <a
+              href='https://www.facebook.com/MusikkapelleMarkelsheim'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <Icon as={FaFacebookSquare} boxSize='24px' color='#3b5998'></Icon>
+            </a>
+            <a
+              href='https://www.instagram.com/musikkapelle_markelsheim'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <Icon
+                boxSize='24px'
+                as={FaInstagram}
+                color='#ffffff'
+                borderRadius='8px'
+                background={
+                  'radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285AEB 90%)'
+                }
+              ></Icon>
+            </a>
+          </HStack>
           <HStack marginY='.8rem' fontSize='.8rem'>
             <Icon as={FaCode}></Icon>
             <Text>with</Text>
