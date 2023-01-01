90cd73be34f712493b51264ac3254774f7f30894
dreitagebart <dreitagebart@users.noreply.github.com>
2023-01-01 19:54:33 +0100

    new flyer & rally removed

--- src/pages/club/recruitment.tsx
+++ src/pages/club/recruitment.tsx
@@ -27,7 +27,7 @@ const Page: NextPageExtended = () => {
           Jugendorchester aufgebaut werden, um das gemeinsame Spielen zu fördern
           und die Freude an der Musik zu verstärken.
         </Block>
-        <Block>
+        {/* <Block>
           Damit sich interessierte Nachwuchsmusiker von morgen in unser
           Vereinsleben einfinden können, bieten wir dieses Jahr am{' '}
           <b>Samstag, den 23.07.2022</b> eine &laquo;Instrumentenrally&raquo; in
@@ -39,7 +39,7 @@ const Page: NextPageExtended = () => {
               Anmeldeformular zur Instrumentenrally
             </Button>
           </Link>
-        </Block>
+        </Block> */}
         <Block>
           Solltest du Interesse an einer Ausbildung haben, so wende dich bitte
           an unseren Vereinsvorstand <Link href='/contact'>Verena Hüttl</Link>.
