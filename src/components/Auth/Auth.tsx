4aef53609f511e6ec87be810f0245cda1d228b39
dreitagebart <input@dreitagebart.io>
2025-02-23 21:38:13 +0100

    ts fix

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -2,7 +2,7 @@ import { Box, Spinner } from '@chakra-ui/react'
 import { useSession } from 'next-auth/react'
 import React from 'react'
 
-interface Props {}
+interface Props { }
 
 export const Auth: React.FC<Props> = ({ children }) => {
   const { data: session, status } = useSession({ required: true })
@@ -20,7 +20,7 @@ export const Auth: React.FC<Props> = ({ children }) => {
       alignItems='center'
       justifyContent='center'
     >
-      <Spinner size='xl'></Spinner>
+      <Spinner size='xl' />
     </Box>
   )
 }
